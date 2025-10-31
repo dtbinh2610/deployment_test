8e624721a213c157cb56bb66dfa7f6a547cf8cea
Đỗ Thanh Bình <dtbinh2610@gmail.com>
2025-10-31 12:03:59 +0700

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,7 +1,7 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello Binh!");
+app.MapGet("/", () => "Hello Do Thanh Binh!");
 
 
 app.Run();
